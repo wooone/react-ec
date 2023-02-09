06dc3b9c72d8a4b432ca9106830c9a6d0566ce66
wooone <yayeahyeahya22@gmail.com>
2023-02-10 01:42:54 +0800

    add products data

--- src/pages/HomePage/HomePageHeroSection.js
+++ src/pages/HomePage/HomePageHeroSection.js
@@ -17,20 +17,17 @@ const HomePageHeroSection = () => {
                   Trending product in {year}
                 </p>
                 <h2 className="text-primary_color text-[1.8rem] md:text-[2.5rem] font-semibold my-[20px] md:leading-12">
-                  Make your Interior More Minimalistic & Modern
+                  和你一起慶祝美好生活
                 </h2>
                 <p className="text-primary_color leading-7">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Vivamus non arcu sit amet nunc suscipit pharetra vel vel dui.
-                  Pellentesque consectetur eu felis sit amet tempus. Etiam a leo
-                  orci.
+                  質感燈具及風格單品，為居家生活增添細膩的感官體驗
                 </p>
 
                 <motion.button
                   whileTap={{ scale: 1.2 }}
                   className="bg-gray-800 text-white px-5 py-2 rounded-full my-[20px] hover:bg-gray-700 transition duration-150 ease-out hover:ease-in"
                 >
-                  <Link to="/shop" className="active:text-white hover:text-white">SHOP NOW</Link>
+                  <Link to="/shop" className="active:text-white hover:text-white">立即購買</Link>
                 </motion.button>
               </div>
             </Col>
